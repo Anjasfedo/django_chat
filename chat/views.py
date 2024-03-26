6720170b2077265b84b27bf21a4f8ec74519f247
AnjasFedo <fedoaf98@gmail.com>
2024-03-27 05:52:04 +0700

    getMessages Fetch Data Every 1 Second

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Room, Message
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from urllib.parse import quote
 
 # Create your views here.
 
@@ -14,7 +15,7 @@ def room(request, room):
     
     context = {
         'username': username,
-        'room': 'room',
+        'room': room,
         'room_details': room_details
     }
     
@@ -41,3 +42,11 @@ def send(request):
     new_message.save
     
     return HttpResponse('Message sent')
+
+def getMessages(request, room):
+    encoded_room = quote(room)
+    room_details = Room.objects.get(name=room)
+    
+    messages = Message.objects.filter(room=room_details.id)
+    
+    return JsonResponse({"messages":list(messages.values())})
