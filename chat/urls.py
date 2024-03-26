3f55c8c22643ce08389ce725e77940856c994d09
AnjasFedo <fedoaf98@gmail.com>
2024-03-27 05:20:24 +0700

    Stucture of Views

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Room, Message
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def room(request, room):
+    context = {}
+    
+    return render(request, 'room.html', context)
+
+def checkview(request):
+    context = {}
+    
+    return render(request, 'template.html', context)
